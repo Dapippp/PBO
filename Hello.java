3d639898382f49d3b041877fac14d7a085f1abea
Davif Adi Prayogo <88437267+Dapippp@users.noreply.github.com>
2023-10-10 14:52:46 +0700

    Update Hello.java
    Davif Adi Prayogo

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
  
 public static void main(String[] args) { 
-           System.out.print("Nama saya Budi!");
+           System.out.print("Nama saya Davif Adi!");
  
      System.out.println("Selamat datang Java!"); 
      }
